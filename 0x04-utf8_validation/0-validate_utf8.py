5849c152dfbbef033aa34e08c1d2e6e5e8d1b400
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-04 19:55:56 +0200

    Task0 fix

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -1,9 +1,10 @@
 #!/usr/bin/python3
 """ UTF 8 validation """
 
+
 def validUTF8(data):
     """ valid utf 8 function """
     for number in data:
-        if (number >= 256):
-            return False            
+        if (number >= 256 and number < 384):
+            return False
     return True
