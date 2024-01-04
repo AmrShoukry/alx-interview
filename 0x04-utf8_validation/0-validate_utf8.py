568fffea85b6721ad8daf1ed5fb4df8e1c1d420f
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-01-04 20:20:37 +0200

    task0 fix3

--- 0x04-utf8_validation/0-validate_utf8.py
+++ 0x04-utf8_validation/0-validate_utf8.py
@@ -7,13 +7,13 @@ def validUTF8(data):
     for number in data:
         if (number < 128):
             continue
-        if number >> 6 == 0b10:
+        if number >> 6 & 0b11 == 0b10:
             continue
-        if number >> 5 == 0b110:
+        if number >> 5 & 0b111 == 0b110:
             continue
-        if number >> 4 == 0b1110:
+        if number >> 4 & 0b1111 == 0b1110:
             continue
-        if number >> 3 == 0b11110:
+        if number >> 3 & 0b11111 == 0b11110:
             continue
         return False
     return True
