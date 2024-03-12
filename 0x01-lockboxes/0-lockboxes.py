6ecf67f1229e78e30f3c9dbd2aafbe4eea21cf6b
Amr Shoukry <shoukryworkamr1@gmail.com>
2024-03-12 13:03:57 +0200

    task0 fix

--- 0x01-lockboxes/0-lockboxes.py
+++ 0x01-lockboxes/0-lockboxes.py
@@ -1,25 +1,25 @@
 #!/usr/bin/python3
 """ Lockboxes """
 
+
 def canUnlockAll(boxes):
     """ Can unlock all """
-    
+
     n = len(boxes)
-    set = {}
+    visited = {}
     for i in range(n):
-        set[i] = 0;
+        visited[i] = 0
     queue = [0]
-    set[0] = 1
-    
+    visited[0] = 1
+
     while queue:
         value = queue.pop(0)
         for key in boxes[value]:
-            if set[key] == 0:
-                set[key] = 1
+            if visited[key] == 0:
+                visited[key] = 1
                 queue.append(key)
-                
-    for (key, value) in set.items():
+
+    for (key, value) in visited.items():
         if (value == 0):
             return False
     return True
-    
